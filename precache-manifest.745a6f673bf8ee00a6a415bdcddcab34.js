88e02279650c07fae30989250cb95d8bdd6a171d
unknown <myrnamares90@gmail.com>
2019-06-26 12:21:27 -0500

    Updates

---
+++ precache-manifest.745a6f673bf8ee00a6a415bdcddcab34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbb66a789b025cebe3c20980cb61ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949419c5585e9c2e51f5",
+    "url": "/static/css/main.0f413419.chunk.css"
+  },
+  {
+    "revision": "4aa66947b3916d1b5ca5",
+    "url": "/static/js/2.9fd80c38.chunk.js"
+  },
+  {
+    "revision": "949419c5585e9c2e51f5",
+    "url": "/static/js/main.edaf9587.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16ce11a15e4c055edb0e059890e12916",
+    "url": "/static/media/escritorio-vintage.16ce11a1.jpg"
+  }
+]);
