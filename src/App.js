2c80bee246704071d55efff29315ab40da9097d2
unknown <myrnamares90@gmail.com>
2019-07-30 13:29:24 -0500

    se guardaron todos los cambis de las carpetas faltantes, por el merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
@@ -38,28 +37,6 @@ class App extends Component{
       )
   }
     
-=======
-import React from 'react';
-import './index.css';
-import {HashRouter,Link, Route, Switch} from 'react-router-dom';
-import Home from "./components/Home";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
-import Button from "react-bootstrap/Button";
-
-function App() {
-  return (
-   <section className="App">
-     <HashRouter>
-    <Switch>
-      <Route path="/" component={Home} exact/>
-      <Route path="/Contact" component={Contact}/>
-      <Route path="/Projects" component={Projects}/>
-    </Switch>
-     </HashRouter>
-   </section>
-  );
->>>>>>> origin/desarrollo
 }
 
 export default App;
